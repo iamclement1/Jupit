90dcf2b782557fef7d4f84093caf715430b2952d
iamclement1 <blessedmarcel1@gmail.com>
2023-01-13 19:25:17 +0100

    feats: first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1 className='text-4xl font-bold'>Welcome to Jupit</h1>
+      <span className='text-2xl'>We'll come back to you</span>
+    </div>
+  )
+}
+
+export default App
