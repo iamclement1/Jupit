a7d14f9e50b011d3be1b2273819b8eb893b65bf6
iamclement1 <blessedmarcel1@gmail.com>
2023-01-14 10:55:11 +0100

    feats: axios & conditional rendering pkg

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
+import { useEffect, useState } from 'react'
+// import { getCoinGecko } from './api/coingecko';
 import './App.css'
+import axios from 'axios';
 
 const App = () => {
 
+  const [coinData, setCoinData] = useState([]);
+  const auth_token = localStorage.getItem('token');
+
+  useEffect(() => {
+    axios.get('https://pro-api.coingecko.com/api/v3/', {
+      headers: {
+        Authorization : 'Bearer ' + auth_token,
+        ContentType: 'application/json'
+      },
+    })
+    .then((response) => {
+      const allCoins = response.data
+      setCoinData(allCoins);
+      console.log(setCoinData(allCoins));
+    })
+    .catch ((error) => {
+      console.error(error);
+    })
+  }, [auth_token])
+
+
+
   return (
     <div className="App">
-      <h1 className='text-4xl font-bold'>Welcome to Jupit</h1>
-      <span className='text-2xl'>We'll come back to you</span>
+      {
+        coinData.map((coin, index) => {
+          return (
+            <div>
+              {data}
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
---
+++ src/api/coingecko.js
@@ -0,0 +1,8 @@
+// // https://pro-api.coingecko.com/api/v3/
+
+// import axios from "axios";
+// const URL = `https://pro-api.coingecko.com/api/v3/`;
+
+// export const getCoinGecko = async () => {
+    
+// }
