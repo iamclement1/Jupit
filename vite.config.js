01b6ffd5637983242d4c41b71b2f5eff62219ca3
iamclement1 <blessedmarcel1@gmail.com>
2023-01-18 13:44:52 +0100

    update: added pwa

--- vite.config.js
+++ vite.config.js
@@ -5,39 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: 'auto',
-    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Jupit',
-        short_name: 'Jupit',
-        description: 'A place for everyone who want to buy and sell digital assets.',
-        theme_color: '#1C1C93',
-        icons: [
-          {
-            src: 'Jupit.svg',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'Jupit.svg',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: "maskable"
-          },
-          {
-            src: 'Jupit.svg',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-    workbox: {
-      clientsClaim: true,
-      skipWaiting: true,
-    },
     devOptions: {
-      enabled: true
+      enabled: true,
     },
-  })],
+    registerType: "autoUpdate",
+  }),],
 })
